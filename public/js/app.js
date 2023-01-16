49f5a7a47259e22e91785826816ea28b09408693
Vincent Garcia <vincentgarciajr2712@gmail.com>
2023-01-15 22:07:19 -0800

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+// fetch("http://localhost:3000/weather?address=Boston").then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
---
+++ src/app.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+//Setup Handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to server CSS and JS
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Vincent Garcia",
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Vincent Garcia",
+  });
+});
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Vincent Garcia",
+    message: "You won't last 30 seconds watching this",
+  });
+});
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.render("error", {
+      title: "Error",
+      errorMessage: "Please provide an address",
+      name: "Vincent Garcia",
+    });
+  }
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.render("error", {
+        title: "Error",
+        errorMessage: "Error invalid address",
+        name: "Vincent Garcia",
+      });
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.render("error", {
+          title: "Error",
+          errorMessage: "Error invalid address",
+          name: "Vincent Garcia",
+        });
+      }
+
+      return res.render("index", {
+        title: "Weather",
+        location: location,
+        forecastData: forecastData,
+        name: "Vincent Garcia",
+      });
+    });
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("error", {
+    title: "Help",
+    errorMessage: "Article not found",
+    name: "Vincent Garcia",
+  });
+});
+app.get("/*", (req, res) => {
+  res.render("error", {
+    title: "Error",
+    errorMessage: "404 Page Not Found",
+    name: "Vincent Garcia",
+  });
+});
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
+});
